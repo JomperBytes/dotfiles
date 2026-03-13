862b546a590ad1a0f4bef4c7f7ad175bef5b8889
José Angel González Rojas <xjagonzalezx@gmail.com>
2026-03-13 08:49:28 +0000

    adding more settings for blink

--- nvim/.config/nvim/lua/custom/plugins/minuet.lua
+++ nvim/.config/nvim/lua/custom/plugins/minuet.lua
@@ -1,5 +1,5 @@
 return {
-  'milanglacier/minuet-ai.nvim', -- <--- ESTO ES LO QUE FALTA
+  'milanglacier/minuet-ai.nvim', --
   dependencies = { 'saghen/blink.cmp' },
   config = function()
     require('minuet').setup {
@@ -7,7 +7,7 @@ return {
       provider_options = {
         openai_compatible = {
           model = 'qwen2.5-coder-3b-instruct-mlx',
-          end_point = 'http://192.168.1.167:1234/v1/chat/completions',
+          end_point = 'http://192.168.1.20:1234/v1/chat/completions',
           stream = true,
           api_key = 'TERM',
         },
@@ -17,12 +17,11 @@ return {
         keymap = {
           -- accept whole completion
           --
-          accept = '<Tab>',
-          -- Otras teclas seguras que no chocan con AeroSpace o Tmux
-          accept_line = '<C-l>',
-          next = '<C-n>',
-          prev = '<C-p>',
-          dismiss = '<C-x>',
+          accept = '<A-y>', -- Atajo interno (puedes no usarlo)
+          accept_line = '<A-l>',
+          next = '<A-n>',
+          prev = '<A-p>',
+          dismiss = '<A-x>',
         },
       },
       trigger = { prefetch_on_insert = true },
